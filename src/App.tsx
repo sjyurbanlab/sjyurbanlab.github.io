a51d11a8113d5c76dc29a0a8170645535811b537
seanchok <seanchok@gmail.com>
2020-06-26 14:08:06 +0800

    add custom colors and font family

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React, { FunctionComponent } from 'react';
 
 export const App: FunctionComponent<void> = () => {
-  return <p className={'text-3xl font-bold'}>hello world</p>;
+  return (
+    <p className={'text-3xl font-bold bg-emerald-green text-white'}>
+      hello world
+    </p>
+  );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+      serif: ['Noto Serif', ...defaultTheme.fontFamily.serif],
+      mono: ['Inconsolata', ...defaultTheme.fontFamily.mono],
+    },
+    extend: {
+      colors: {
+        white: '#FFF',
+        'emerald-green': '#50C878',
+        'cobalt-blue': '#1446A0',
+        'razzmatazz-red': '#DB3069',
+        'mustard-yellow': '#F5D547',
+        'jet-black': '#3C3C3B',
+      },
+    },
   },
   variants: {},
   plugins: [],
