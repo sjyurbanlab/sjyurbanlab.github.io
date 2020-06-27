bc1e844f8b8da3475bb9f933625eaf61dd2acbda
seanchok <seanchok@gmail.com>
2020-06-27 22:59:50 +0800

    make css markup for generic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import React, { FunctionComponent } from 'react';
+import { PageContainer } from './components/layout';
 
 export const App: FunctionComponent<void> = () => {
   return (
-    <p className={'text-3xl font-bold bg-emerald-green text-white'}>
-      hello world
-    </p>
+    <PageContainer>
+      <h1>Large heading</h1>
+      <h2>Sub heading</h2>
+      <h3>smaller heading</h3>
+      <h4>smallest heading</h4>
+      <p className={'text-sm'}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. <a href={'/'}>Lorem Ipsum</a> has been the industry's standard
+        dummy text ever since the 1500s, when an unknown printer took a galley
+        of type and scrambled it to make a type specimen book. It has survived
+        not only <a href={'/'}>five centuries</a>, but also the leap into
+        electronic typesetting, remaining essentially unchanged. It was
+        popularised in the 1960s with the{' '}
+        <a href={'/'}>
+          release of Letraset sheets containing Lorem Ipsum passages
+        </a>
+        , and more recently with desktop publishing software like Aldus
+        PageMaker including versions of Lorem Ipsum.
+      </p>
+      <button className={'button button-emerald-green'}>click me</button>
+    </PageContainer>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,43 @@ module.exports = {
       serif: ['Noto Serif', ...defaultTheme.fontFamily.serif],
       mono: ['Inconsolata', ...defaultTheme.fontFamily.mono],
     },
+    container: {
+      center: true,
+      padding: {
+        default: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+      },
+    },
     extend: {
       colors: {
         white: '#FFF',
-        'emerald-green': '#50C878',
-        'cobalt-blue': '#1446A0',
-        'razzmatazz-red': '#DB3069',
-        'mustard-yellow': '#F5D547',
-        'jet-black': '#3C3C3B',
+        'emerald-green': {
+          light: '#64CE87',
+          default: '#50C878',
+          dark: '#45C46F',
+        },
+        'cobalt-blue': {
+          light: '#164EB6',
+          default: '#1446A0',
+          dark: '#123F91',
+        },
+        'razzmatazz-red': {
+          light: '#DD4075',
+          default: '#DB3069',
+          dark: '#D0255E',
+        },
+        'mustard-yellow': {
+          light: '#F7DC64',
+          default: '#F5D547',
+          dark: '#F5D33D',
+        },
+        'jet-black': {
+          light: '#484847',
+          default: '#3C3C3B',
+          dark: '#343432',
+        },
       },
     },
   },
