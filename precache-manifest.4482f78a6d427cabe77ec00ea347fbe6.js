4b6461326719a303e28a08b1bc16731d974abd8f
98sean98 <seanchok@gmail.com>
2020-07-06 10:01:33 +0000

    Deploying to master from  @ 6e61ec8b73903406e283c7c9d69e216495b1ef12 🚀

---
+++ precache-manifest.4482f78a6d427cabe77ec00ea347fbe6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bff43021890b58b9d274c81f1f64eab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59df751b2eedf87eed4c",
+    "url": "/static/js/2.cf4066c7.chunk.js"
+  },
+  {
+    "revision": "a13369e7405157f254f1ffcc4cc651e5",
+    "url": "/static/js/2.cf4066c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e311bb1ff26b57de8202",
+    "url": "/static/js/main.b674a341.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
