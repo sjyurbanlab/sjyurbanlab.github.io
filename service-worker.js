8145ca2f5ee85564179baa29c623a8f9accff690
98sean98 <seanchok@gmail.com>
2020-07-08 14:58:04 +0000

    Deploying to master from  @ fc99e43209dc32f8b901740b3f412d7b9cc33d92 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e8a161dd939e01dffa873cd62df127fb.js"
+  "/precache-manifest.8465e90dbce9aa9b4ef46f4b539dad28.js"
 );
 
 self.addEventListener('message', (event) => {
