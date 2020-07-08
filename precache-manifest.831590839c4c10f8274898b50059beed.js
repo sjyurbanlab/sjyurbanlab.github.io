f5095b687f87493d0d5dbb6c3a144780665e6e15
98sean98 <seanchok@gmail.com>
2020-07-08 09:53:11 +0000

    Deploying to master from  @ 29d559a6257073baa68fc43c6468a6913ba98d8b 🚀

---
+++ precache-manifest.831590839c4c10f8274898b50059beed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b140e9d828bf5c37294c8a525ac63d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "758f2c31fffe74593a39",
+    "url": "/static/js/2.1e5d171b.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.1e5d171b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc42fda07df4ca317d74",
+    "url": "/static/js/main.0356f797.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d265745633b0746552b89af7a73e5b26.js"
+  "/precache-manifest.831590839c4c10f8274898b50059beed.js"
 );
 
 self.addEventListener('message', (event) => {
