938d8633df1c48f30968666a050209f8cf480c52
98sean98 <seanchok@gmail.com>
2020-08-04 08:50:23 +0000

    Deploying to master from  @ 0c53886758e3f0e51d9f9b4ac11cc51009db61f6 🚀

---
+++ component---src-pages-research-past-projects-test-2-mdx-f3ab7f32819164253a63.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{Ac5t:function(t,n,e){"use strict";e.r(n),e.d(n,"_frontmatter",(function(){return p})),e.d(n,"default",(function(){return a}));var o=e("zLVn"),c=(e("q1tI"),e("7ljp")),r=e("NsgW"),p={},s={_frontmatter:p},u=r.b;function a(t){var n=t.components,e=Object(o.a)(t,["components"]);return Object(c.b)(u,Object.assign({},s,e,{components:n,mdxType:"MDXLayout"}),Object(c.b)("p",null,"test page 2"))}a.isMDXComponent=!0}}]);
+//# sourceMappingURL=component---src-pages-research-past-projects-test-2-mdx-f3ab7f32819164253a63.js.map
